51c99b14df446b70bb097c967971f9268d7460ab
laith-hayajneh <farfesh96@gmail.com>
2021-08-03 13:14:47 +0300

    laith

--- src/AllDataAPI.js
+++ src/AllDataAPI.js
@@ -1,13 +1,70 @@
 import React, { Component } from 'react';
 import { withAuth0 } from '@auth0/auth0-react';
-
-
+import axios from "axios"
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
+import { useAuth0 } from "@auth0/auth0-react";
 class AllDataAPI extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            color: [],
+            profileData:[]
+        }
+    }
+    // const user=this.props.auth0;
+    componentDidMount = async () => {
+        axios.get(`${process.env.REACT_APP_SERVER}/getAPIData`).then(result => {
+            this.setState({
+                color: result.data
+            })
+        })
+        console.log('asdasdas', this.state.color);
+    }
+    addColor=async(index)=>{
+        console.log('indexxx',index)
+        console.log(this.state.color[index],'sasdttttt')
+        const colorData={
+            colorName:this.state.color[index].name,
+            colorImage:this.state.color[index].image
+        }
+        axios.post(`${process.env.REACT_APP_SERVER}/getAPIData?email=${this.props.auth0.user.email}`,colorData);
+        console.log('eeeeeeeee',this.props.auth0.user.email)
+
+        console.log('runnnnn',colorData)
+    }
+
+   
+
+
+
+
     render() {
+        console.log('asdasdas', this.state.color)
+
         return (
             <div>
                 <h1>All Data from the API</h1>
                 <h3>Select your favorites :)</h3>
+
+
+                {this.state.color.map((element,index)=>{
+                    return (
+                        <div key={index} className="laith">
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={element.image} />
+                            <Card.Body>
+                                <Card.Title>{element.name}</Card.Title>
+                                
+                                <Button onClick={()=>{this.addColor(index)}} variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </div>
+
+                    )
+                })}
+               
             </div>
         )
     }
--- src/MyFavorites.js
+++ src/MyFavorites.js
@@ -2,15 +2,70 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './MyFavorites.css';
 import { withAuth0 } from '@auth0/auth0-react';
-
+import axios from 'axios';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
 class MyFavorites extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      profileData: []
+    }
+  }
+
+
+
+
+  // http://localhost:3001/?email=laithhayajneh98@gmail.com
+  componentDidMount = async () => {
+    console.log('semmmmm', this.props.auth0.user.email)
+    let server = process.env.REACT_APP_SERVER;
+    console.log('seeerver', server)
+    let profileRead = await axios.get(`${server}/?email=${this.props.auth0.user.email}`);
+    console.log('profile read', profileRead)
+    await this.setState({
+      profileData: profileRead.data
+    })
+    console.log('this is profile data', this.state.profileData)
+  }
+
+
+
+  deleteHandler = async (id) => {
+    let server = process.env.REACT_APP_SERVER;
+    const colorData = await axios.delete(`${server}/${id}?email=${this.props.auth0.user.email}`);
+    this.setState({
+      profileData: colorData.data
+    })
+  }
+
+
   render() {
-    return(
+    return (
       <>
         <h1>My Favorites</h1>
         <p>
           This is a collection of my favorites
         </p>
+
+        {this.state.profileData.map((element, index) => {
+          return (
+            <div key={index} className="laith">
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={element.image} />
+                <Card.Body>
+                  <Card.Title>{element.name}</Card.Title>
+
+                  <Button onClick={() => { this.deleteHandler(index) }} variant="primary">delete</Button>
+                </Card.Body>
+              </Card>
+            </div>
+
+          )
+        })}
+
       </>
     )
   }
